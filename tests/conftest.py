67c5057cb31172f95e762230eef6fe73b290070e
Kirill Ulyanov <ulyanov.k.mgn@gmail.com>
2025-03-25 17:59:44 +0300

    v2

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,16 @@ import src.helpers as helpers
 @pytest.fixture
 def use_courier_data():
     courier_data = helpers.create_new_courier()
-    yield courier_data
+    return courier_data
     helpers.delete_courier(courier_data)
     
 @pytest.fixture
 def use_created_courier_data():
     courier_data = helpers.create_new_courier(register=True)
-    yield courier_data
+    return courier_data
     helpers.delete_courier(courier_data)
 
 @pytest.fixture
 def use_created_courier_data_for_deletion():
     courier_data = helpers.create_new_courier(register=True)
-    yield courier_data
+    return courier_data
--- tests/test_delete_courier.py
+++ tests/test_delete_courier.py
@@ -3,6 +3,7 @@ import src.helpers as helpers
 import src.data as data
 import src.api_requests as api
 
+
 @allure.suite('Тесты удаления курьера')
 class TestDeleteCourier:
     @allure.title('Успешное удаление курьера через ручку /api/v1/courier/:id')
@@ -10,26 +11,49 @@ class TestDeleteCourier:
         login_data = helpers.make_login_data(use_created_courier_data_for_deletion)
         courier_id = api.login_courier(login_data).json()['id']
         delete_courier_response = api.delete_courier(courier_id)
+
+        # Проверка статуса кода
         assert delete_courier_response.status_code == 200
-        assert 'ok' in delete_courier_response.json() and delete_courier_response.json()['ok'] is True
-    
+
+        # Парсинг ответа
+        response_json = delete_courier_response.json()
+
+        # Проверка содержимого ответа
+        assert 'ok' in response_json
+        assert response_json['ok'] is True
+
     @allure.title('Удаление курьера без указания id через ручку /api/v1/courier/:id')
     def test_delete_courier_without_id_in_url_shows_missing_id_error(self):
         courier_id = ''
         delete_courier_response = api.delete_courier(courier_id)
+
+        # Проверка статуса кода
         assert delete_courier_response.status_code == 400
-        assert 'message' in delete_courier_response.json()
-        assert delete_courier_response.json()['message'] == data.DELETE_WITH_EMPTY_ID_ERROR_MESSAGE
-    
+
+        # Парсинг ответа
+        response_json = delete_courier_response.json()
+
+        # Проверка содержимого ответа
+        assert 'message' in response_json
+        assert response_json['message'] == data.DELETE_WITH_EMPTY_ID_ERROR_MESSAGE
+
     @allure.title('Удаление курьера с указанием несуществующего id через ручку /api/v1/courier/:id')
     def test_delete_courier_twice_shows_incorrect_id_error(self, use_created_courier_data_for_deletion):
         login_data = helpers.make_login_data(use_created_courier_data_for_deletion)
         courier_id = api.login_courier(login_data).json()['id']
         delete_courier_response = api.delete_courier(courier_id)
+
+        # Проверка статуса кода
         assert delete_courier_response.status_code == 200
-        
+
         second_delete_courier_response = api.delete_courier(courier_id)
+
+        # Проверка статуса кода
         assert second_delete_courier_response.status_code == 404
-        assert 'message' in second_delete_courier_response.json()
-        assert second_delete_courier_response.json()['message'] == data.DELETE_WITH_UNEXISTING_ID_ERROR_MESSAGE
-        
+
+        # Парсинг ответа
+        response_json = second_delete_courier_response.json()
+
+        # Проверка содержимого ответа
+        assert 'message' in response_json
+        assert response_json['message'] == data.DELETE_WITH_UNEXISTING_ID_ERROR_MESSAGE
